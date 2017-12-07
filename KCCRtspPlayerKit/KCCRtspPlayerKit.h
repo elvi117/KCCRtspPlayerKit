9abdbbb6f24de39315849ce5bfc7d7882a5a3711
Lukasz Matuszczak <lukasz.matuszczak@appunite.com>
2017-12-07 10:45:38 +0100

    Initial Commit

---
+++ KCCRtspPlayerKit/KCCRtspPlayerKit.h
@@ -0,0 +1,19 @@
+//
+//  KCCRtspPlayerKit.h
+//  KCCRtspPlayerKit
+//
+//  Created by Łukasz Matuszczak on 07/12/2017.
+//  Copyright © 2017 AppUnite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCCRtspPlayerKit.
+FOUNDATION_EXPORT double KCCRtspPlayerKitVersionNumber;
+
+//! Project version string for KCCRtspPlayerKit.
+FOUNDATION_EXPORT const unsigned char KCCRtspPlayerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCCRtspPlayerKit/PublicHeader.h>
+
+
